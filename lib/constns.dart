f7f3e9618d15d9a65611f01e52e1b97b4c127bee
ascom <m.sabry1110@gmail.com>
2024-03-16 23:43:51 +0200

    refactor init state&navigateto home

--- lib/constns.dart
+++ lib/constns.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kprimarycolor = Color(0xff100B20);
+const KtranstionDuration = Duration(milliseconds: 250);
--- lib/features/splash/presentation/views/widget/splash_view_body.dart
+++ lib/features/splash/presentation/views/widget/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly/constns.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/home/data/presention/view_models/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/widget/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewbody extends StatefulWidget {
   const SplashViewbody({super.key});
@@ -14,14 +18,11 @@ class _SplashViewbodyState extends State<SplashViewbody>
   late AnimationController animationController;
   late Animation<Offset> slidingAnimation;
   @override
-  void initState() {
+  void iniState() {
     super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 8), end: Offset.zero)
-            .animate(animationController);
-    animationController.forward();
+    initSlidingAnimation();
+
+    navigateToHome();
   }
 
   @override
@@ -44,5 +45,26 @@ class _SplashViewbodyState extends State<SplashViewbody>
         SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
+  }
+
+  void initState() {
+    initSlidingAnimation();
+  }
+
+  void initSlidingAnimation() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 8), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => HomeView(),
+          transition: Transition.fade, duration: KtranstionDuration);
+    });
   }
 }
